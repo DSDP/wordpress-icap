c4f72379c388c49cfde7e70d55941a1c2e53bd25
santuan <smrcouto88@gmail.com>
2015-07-17 17:42:04 -0300

    Initial commit

---
+++ themes/Icap-theme/page-home.php
@@ -0,0 +1,27 @@
+<?php get_header(); ?>
+
+<div class="home-hero">
+    <div class="container ">
+        <div class="row">
+            <div class="col-md-6">a</div>
+            <div class="col-md-6">a</div>
+        </div>
+    </div>
+</div>
+
+<div class="home-widgets">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">a</div>
+            <div class="col-md-3">a</div>
+            <div class="col-md-3">a</div>
+            <div class="col-md-3">
+                <img src="http://localhost/WP/wp-content/uploads/2015/07/conversasiones.png" alt="">
+                <hr>
+                <img src="http://localhost/WP/wp-content/uploads/2015/07/democracia-late.png" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php get_footer();
